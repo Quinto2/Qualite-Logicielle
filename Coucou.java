2ea1d1cef8e6cf31cb8ea5494d23799ff13a2f53
Clément Guillaume <clementguillaume56@gmail.com>
2014-02-11 15:21:58 +0100

    add semicolon
    compile

--- Coucou.java
+++ Coucou.java
@@ -3,6 +3,6 @@ public class Coucou {
 	public static void main(String[] args) {
 		int i = 0;
 		while (i++ < 10)
-			System.out.println("coucou " + i);
+			System.out.println("coucou : " + i);
 	}
 }
