44f22c62e6774939068fc9c49e34ffffcc0b976e
Clément Guillaume <clementguillaume56@gmail.com>
2014-02-11 14:56:29 +0100

    java code style

--- Hail.java
+++ Hail.java
@@ -1,7 +1,6 @@
 public class Coucou {
 
-  public static void main(String[] args) {
-  
-    System.out.println("coucou");
-    }
-  }
+	public static void main(String[] args) {
+		System.out.println("coucou");
+	}
+}
